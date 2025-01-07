190dc98d447dfadb54d3627bfd54a937eb855749
Juan Ignacio García <juanigarciadev@gmail.com>
2025-01-07 17:07:45 -0300

    Se añade la anulación de impuestos dependiendo de si la divisa lo requiere o no

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,14 +1,20 @@
 "use client"
 import { useEffect, useMemo, useState } from "react"
 import CopyIcon from "../icons/CopyIcon"
+import CardIcon from "@/icons/CardIcon";
+import CryptoIcon from "@/icons/CryptoIcon";
+import BankIcon from "@/icons/BankIcon";
 
 export default function Calculator() {
 
     // Se setean los valores de los dólares
     const [precioDolarInicial, setPrecioDolarInicial] = useState(0);
     const [precioDolar, setPrecioDolar] = useState(0);
-    const [precioDolarBlue, setPrecioDolarBlue] = useState(0);
+    // const [precioDolarBlue, setPrecioDolarBlue] = useState(0);
     const [precioDolarCrypto, setPrecioDolarCrypto] = useState(0);
+
+    // Estado para la divisa seleccionada
+    const [divisaSeleccionada, setDivisaSeleccionada] = useState('Dólar Tarjeta');
 
     useEffect(() => {
         fetch('https://dolarapi.com/v1/dolares/oficial')
@@ -18,9 +24,9 @@ export default function Calculator() {
                 setPrecioDolarInicial(data.venta);
             });
 
-        fetch('https://dolarapi.com/v1/dolares/blue')
-            .then(response => response.json())
-            .then(data => setPrecioDolarBlue(data.venta));
+        // fetch('https://dolarapi.com/v1/dolares/blue')
+        //     .then(response => response.json())
+        //     .then(data => setPrecioDolarBlue(data.venta));
 
         fetch('https://dolarapi.com/v1/dolares/cripto')
             .then(response => response.json())
@@ -31,8 +37,13 @@ export default function Calculator() {
     const [value, setValue] = useState(0)
 
     // Se crean los valores de los impuestos basado en el valor ingresado en el input, el precio del dolar y el porcentaje impositivo
-    const iva = useMemo(()=> Math.round(value*precioDolar*0.21), [value, precioDolar])
-    const ganancias = useMemo(()=> Math.round(value*precioDolar*0.30), [value, precioDolar])
+    const iva = useMemo(()=> {
+        return divisaSeleccionada === 'Dólar Crypto' ? 0 : Math.round(value * precioDolar * 0.21)
+    }, [value, precioDolar, divisaSeleccionada])
+
+    const ganancias = useMemo(()=> {return divisaSeleccionada === 'Dólar Crypto' || 'Dólar MEP' ? 0 : Math.round(value * precioDolar * 0.21)
+    }, [value, precioDolar, divisaSeleccionada])
+
     const impuestosTotales = useMemo(()=> Math.round(iva + ganancias), [iva, ganancias])
     const totalValue = useMemo(()=> value*precioDolar + impuestosTotales, [value, precioDolar, impuestosTotales])
 
@@ -41,6 +52,7 @@ export default function Calculator() {
     
     function updateCurrency(currencyName: string, currencyValue: number): void {
         setDropdownTitle(currencyName);
+        setDivisaSeleccionada(currencyName);
         if (currencyName === 'Dólar Tarjeta') {
           setPrecioDolar(precioDolarInicial);
         } else {
@@ -58,21 +70,24 @@ export default function Calculator() {
     
     return(
         <section className="flex flex-col gap-4 w-full">
-            <div className="w-full py-2 px-6 border border-neutral-300 rounded-lg cursor-pointer hover:bg-neutral-100 duration-200 relative" onClick={()=>setDropdownToggle(!dropdownToggle)}>
+            <div className="w-full py-2 px-6 border border-neutral-300 rounded-lg cursor-pointer hover:bg-neutral-100 duration-200 relative " onClick={()=>setDropdownToggle(!dropdownToggle)}>
                 <span className="text-sm text-neutral-500 select-none">{dropdownTitle}</span>
                 {dropdownToggle ? <div className="absolute bg-white w-full top-12 left-0 border border-neutral-300 shadow-sm rounded-lg">
-                    <div className="w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Tarjeta', precioDolarInicial)}>
+                    <div className="inline-flex gap-2 items-center w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Tarjeta', precioDolarInicial)}>
+                        <CardIcon/>
                         <span className="text-sm text-neutral-500">Dólar Tarjeta</span>
                     </div>
-                    <div className="w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>setDropdownTitle('Dólar MEP')}>
+                    <div className="inline-flex gap-2 items-center w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar MEP', precioDolarCrypto)}>
+                        <BankIcon/>
                         <span className="text-sm text-neutral-500">Dólar MEP</span>
                     </div>
-                    <div className="w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Crypto', precioDolarCrypto)}>
+                    <div className="inline-flex gap-2 items-center w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Crypto', precioDolarCrypto)}>
+                        <CryptoIcon/>
                         <span className="text-sm text-neutral-500">Dólar Crypto</span>
                     </div>
-                    <div className="w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Blue', precioDolarBlue)}>
+                    {/* <div className="inline-flex gap-2 items-center w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Blue', precioDolarBlue)}>
                         <span className="text-sm text-neutral-500">Dólar Blue</span>
-                    </div>
+                    </div> */}
                 </div> : null}
             </div>
             <input onChange={(e)=>setValue(Number(e.target.value))} type="number" className="calculatorInput font-medium rounded-full h-12 w-full py-2 px-6 border-2 border-neutral-300 hover:border-neutral-400 focus:border-neutral-600 duration-200" placeholder="0.00"/>
