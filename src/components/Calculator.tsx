a65b04188763e3e163d6b595c04bd7ee403fa8b4
Juan Ignacio García <juanigarciadev@gmail.com>
2025-01-07 17:56:10 -0300

    Se agrega selector de divisas (sin funcionamiento) y se agrega un indicador de flecha con animacion en el desplegable de selección de tipo de dólar

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,17 +1,18 @@
 "use client"
-import { useEffect, useMemo, useState } from "react"
+import { FormEvent, useEffect, useMemo, useState } from "react"
 import CopyIcon from "../icons/CopyIcon"
 import CardIcon from "@/icons/CardIcon";
 import CryptoIcon from "@/icons/CryptoIcon";
 import BankIcon from "@/icons/BankIcon";
+import ChevronDown from "@/icons/ChevronDown";
 
 export default function Calculator() {
 
     // Se setean los valores de los dólares
     const [precioDolarInicial, setPrecioDolarInicial] = useState(0);
     const [precioDolar, setPrecioDolar] = useState(0);
-    // const [precioDolarBlue, setPrecioDolarBlue] = useState(0);
     const [precioDolarCrypto, setPrecioDolarCrypto] = useState(0);
+    // const [precioDolarBlue, setPrecioDolarBlue] = useState(0);
 
     // Estado para la divisa seleccionada
     const [divisaSeleccionada, setDivisaSeleccionada] = useState('Dólar Tarjeta');
@@ -48,6 +49,7 @@ export default function Calculator() {
     const totalValue = useMemo(()=> value*precioDolar + impuestosTotales, [value, precioDolar, impuestosTotales])
 
     const [dropdownToggle, setDropdownToggle] = useState(false)
+    const [dropdownCurrencyToggle, setDropdownCurrencyToggle] = useState(false)
     const [dropdownTitle, setDropdownTitle] = useState('Dólar Tarjeta')
     
     function updateCurrency(currencyName: string, currencyValue: number): void {
@@ -67,12 +69,24 @@ export default function Calculator() {
     const copyToClipboard = (text: string) => {
         navigator.clipboard.writeText(text)
     }
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+    }
+
+    const dropdownArrowRotation = dropdownToggle ? 'rotate-[180deg]' : null
     
     return(
         <section className="flex flex-col gap-4 w-full">
-            <div className="w-full py-2 px-6 border border-neutral-300 rounded-lg cursor-pointer hover:bg-neutral-100 duration-200 relative " onClick={()=>setDropdownToggle(!dropdownToggle)}>
+            <div className="w-full py-2 px-6 border border-neutral-300 rounded-lg cursor-pointer hover:bg-neutral-100 duration-200 relative " onClick={()=>{
+                setDropdownToggle(!dropdownToggle)
+                setDropdownCurrencyToggle(false)
+                }}>
                 <span className="text-sm text-neutral-500 select-none">{dropdownTitle}</span>
-                {dropdownToggle ? <div className="absolute bg-white w-full top-12 left-0 border border-neutral-300 shadow-sm rounded-lg">
+                <span className={`right-4 top-2 absolute select-none duration-200 ${dropdownArrowRotation}`}>
+                    <ChevronDown/>
+                </span>
+                {dropdownToggle ? <div className="z-20 absolute bg-white w-full top-12 left-0 border border-neutral-300 shadow-sm rounded-lg">
                     <div className="inline-flex gap-2 items-center w-full hover:bg-neutral-100 py-4 px-6 select-none" onClick={()=>updateCurrency('Dólar Tarjeta', precioDolarInicial)}>
                         <CardIcon/>
                         <span className="text-sm text-neutral-500">Dólar Tarjeta</span>
@@ -90,7 +104,26 @@ export default function Calculator() {
                     </div> */}
                 </div> : null}
             </div>
-            <input onChange={(e)=>setValue(Number(e.target.value))} type="number" className="calculatorInput font-medium rounded-full h-12 w-full py-2 px-6 border-2 border-neutral-300 hover:border-neutral-400 focus:border-neutral-600 duration-200" placeholder="0.00"/>
+            <form onSubmit={handleSubmit} className="relative">
+                <div className="inline-flex items-center justify-center absolute bg-white border border-neutral-300 w-10 h-6 rounded-lg select-none cursor-pointer right-4 top-[11px] hover:bg-neutral-100" onClick={()=>{
+                    setDropdownCurrencyToggle(!dropdownCurrencyToggle)
+                    setDropdownToggle(false)
+                }}>
+                    {dropdownCurrencyToggle ? <div className="bg-white z-10 border border-neutral-300 rounded-lg absolute right-0 top-7">
+                        <div className="inline-flex items-center w-full py-1 px-6 select-none hover:bg-neutral-100">
+                            <span className="text-xs text-neutral-500">USD</span>
+                        </div>
+                        <div className="inline-flex items-center w-full py-1 px-6 select-none hover:bg-neutral-100 cursor-not-allowed">
+                            <span className="text-xs text-neutral-500">EUR</span>
+                        </div>
+                        <div className="inline-flex items-center w-full py-1 px-6 select-none hover:bg-neutral-100 cursor-not-allowed">
+                            <span className="text-xs text-neutral-500">ARS</span>
+                        </div>
+                    </div> : null}
+                    <span className="text-xs text-neutral-500">USD</span>
+                </div>
+                <input onChange={(e)=>setValue(Number(e.target.value))} type="number" className="font-medium rounded-full h-12 w-full py-2 px-6 pr-16 border-2 border-neutral-300 hover:border-neutral-400 focus:border-neutral-600 duration-200" placeholder="0.00"/>
+            </form>
             <div className="flex flex-col gap-2">
                 <div className="flex justify-between">
                     <span className="font-medium">IVA 21%</span>
