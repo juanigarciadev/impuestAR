f1fa2f6aa94bc8fc76609e5f5e15ed74873d78d0
Juan Ignacio García <juanigarciadev@gmail.com>
2025-01-08 23:47:23 -0300

    Se corrige error en el color fill de dos iconos svg que generaban un mal contraste en modo claro

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -2,7 +2,7 @@ import CoffeeIcon from "@/icons/CoffeeIcon";
 
 export default function SupportBanner() {
     return(
-        <a href="https://cafecito.app/juanigarciadev" target="_blank" className="inline-flex gap-1 items-center justify-center w-full rounded-lg py-4 cursor-pointer bg-[linear-gradient(60deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))] hover:bg-[linear-gradient(150deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))]">
+        <a href="https://cafecito.app/juanigarciadev" target="_blank" className="text-white inline-flex gap-1 items-center justify-center w-full rounded-lg py-4 cursor-pointer bg-[linear-gradient(60deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))] hover:bg-[linear-gradient(150deg,_rgb(247,_149,_51),_rgb(243,_112,_85),_rgb(239,_78,_123),_rgb(161,_102,_171),_rgb(80,_115,_184),_rgb(16,_152,_173),_rgb(7,_179,_155),_rgb(111,_186,_130))]">
             <CoffeeIcon/>
             <h3 className="text-white text-sm">Apoyá el proyecto</h3>
         </a>
--- src/components/UpdateRequest.tsx
+++ src/components/UpdateRequest.tsx
@@ -2,7 +2,7 @@ import SendIcon from "@/icons/SendIcon";
 
 export default function UpdateRequest() {
     return(
-        <a href="mailto:juanigarciadev@gmail.com" target="_blank" className="inline-flex gap-1 items-center justify-center w-full rounded-lg py-4 cursor-pointer bg-blue-600 hover:bg-blue-700 duration-200">
+        <a href="mailto:juanigarciadev@gmail.com" target="_blank" className="text-white inline-flex gap-1 items-center justify-center w-full rounded-lg py-4 cursor-pointer bg-blue-600 hover:bg-blue-700 duration-200">
             <SendIcon/>
             <h3 className="text-white text-sm">Solicitar una revisión</h3>
         </a>
